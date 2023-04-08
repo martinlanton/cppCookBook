3cd9bbddb59f50b60359a731cadda15345d6fd07
martinlanton <lantonmartin@gmail.com>
2023-04-08 14:08:50 -0700

    Adding use of console input

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using std::cout;
+using std::cin;
 
 
 int main() {
     cout << "Hello, World!" << std::endl;
     cout << "This is awesome!" << std::endl;
-    int first = 32;
-    int second = 48;
+    int first = 0;
+    int second = 0;
+    cout << "Enter your first number :" << std::endl;
+    cin >> first;
+    cout << "Enter your second number :" << std::endl;
+    cin >> second;
+    cout << "Total is :" << std::endl;
     int total = first + second;
     cout << total << std::endl;
     cout << '\n';
