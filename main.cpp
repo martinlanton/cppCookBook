4ebf28ff94eeb321510e80c91969f8302872e00c
martinlanton <lantonmartin@gmail.com>
2023-04-08 14:10:07 -0700

    Adding use of comments

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using std::cin;
 
 
 int main() {
-    cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;  // cout stands for console output
     cout << "This is awesome!" << std::endl;
     int first = 0;
     int second = 0;
     cout << "Enter your first number :" << std::endl;
-    cin >> first;
+    cin >> first;  // cin stands for console input
     cout << "Enter your second number :" << std::endl;
     cin >> second;
     cout << "Total is :" << std::endl;
