0319b7f45d3efd0fbb668b1af149721eddbc1da4
martinlanton <lantonmartin@gmail.com>
2023-04-08 14:03:47 -0700

    Adding use of local variables and integers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+using std::cout;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "This is awesome!" << std::endl;
+    cout << "Hello, World!" << std::endl;
+    cout << "This is awesome!" << std::endl;
+    int first = 32;
+    int second = 48;
+    int total = first + second;
+    cout << total << std::endl;
+    cout << '\n';
     return 0;
 }
