80bee39b84f471139a776be01d2bcbdde50350f6
martinlanton <lantonmartin@gmail.com>
2023-04-13 14:47:23 -0700

    Added some work with strings

--- 04_strings_and_collections/main.cpp
+++ 04_strings_and_collections/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <string>
+using std::string;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string firstPhrase;
+    string secondPhrase;
+
+    std::cout << "Enter the first phrase : " << std::endl;
+    std::getline(std::cin, firstPhrase);
+    std::cout << "Enter the second phrase : " << std::endl;
+    std::getline(std::cin, secondPhrase);
+    int firstLength = firstPhrase.length();
+    int secondLength = secondPhrase.length();
+
+    if (firstLength == secondLength){
+        std::cout << "Both phrases are the same length." << std::endl;
+        return 0;
+    }
+    if (firstLength > secondLength){
+        std::cout << "The first phrase is longer than the second";
+    }
+    else {
+        std::cout << "The first phrase is shorter than the second";
+    }
     return 0;
 }
