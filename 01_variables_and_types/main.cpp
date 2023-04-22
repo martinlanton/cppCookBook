4aba5719ce20e027bbee39c6f2ba62d2264dcacb
martinlanton <lantonmartin@gmail.com>
2023-04-21 17:12:46 -0700

    Added comments to explain basic cpp syntax

--- 01_variables_and_types/main.cpp
+++ 01_variables_and_types/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+// comments are declared after a double / sign, like at the start of this very line
+// all lines must be finished by a semicolon, unless they're declaration lines, like functions, etc.
+// the "using" statement indicates to the compiler that it should try to assign the calls to that
+// namespace first, in this case that cout calls should be assigned to std::cout
 using std::cout;
 using std::cin;
 
 
+// to declare a function, we first declare the return type, then the name of the function, then the
+// arguments between parentheses, finally we open a curly bracket for the function's body
 int main() {
-    cout << "Hello, World!" << std::endl;  // cout stands for console output
+    // cout stands for console output
+    // the << symbol is used to send something to a stream, in this case the cout stream
+    cout << "Hello, World!" << std::endl;
     cout << "This is awesome!" << std::endl;
+    // to declare variables, declare type first, then variable name, then,
+    // optionally, assign a value
     int first = 0;
     int second = 0;
     cout << "Enter your first number :" << std::endl;
