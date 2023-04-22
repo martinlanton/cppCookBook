6ae39120fd9c292302dd831a1fb50398dec7bb0e
martinlanton <lantonmartin@gmail.com>
2023-04-21 17:22:38 -0700

    Added comments to explain basic cpp syntax

--- 01_variables_and_types/main.cpp
+++ 01_variables_and_types/main.cpp
@@ -1,24 +1,24 @@
-// comments are declared after a double / sign, like at the start of this very line
-// include statements start with an # sign and are telling the compiler where to find the
+// Comments are declared after a double / sign, like at the start of this very line
+// Include statements start with an # sign and are telling the compiler where to find the
 // definitions that this file relies on, those could be in a library (like here), a different file,
 // or a header (see chapter 3 : 03_functions_and_headers), they're "basically" the equivalent of a
 // python import statement
 #include <iostream>
-// all lines must be finished by a semicolon, unless they're declaration lines, like functions, etc.
-// the "using" statement indicates to the compiler that it should try to assign the calls to that
+// All lines must be finished by a semicolon, unless they're declaration lines, like functions, etc.
+// The "using" statement indicates to the compiler that it should try to assign the calls to that
 // namespace first, in this case that cout calls should be assigned to std::cout
 using std::cout;
 using std::cin;
 
 
-// to declare a function, we first declare the return type, then the name of the function, then the
+// To declare a function, we first declare the return type, then the name of the function, then the
 // arguments between parentheses, finally we open a curly bracket for the function's body
 int main() {
     // cout stands for console output
-    // the << symbol is used to send something to a stream, in this case the cout stream
+    // The << symbol is used to send something to a stream, in this case the cout stream
     cout << "Hello, World!" << std::endl;
     cout << "This is awesome!" << std::endl;
-    // to declare variables, declare type first, then variable name, then,
+    // To declare variables, declare type first, then variable name, then,
     // optionally, assign a value
     int first = 0;
     int second = 0;
@@ -30,5 +30,6 @@ int main() {
     int total = first + second;
     cout << total << std::endl;
     cout << '\n';
+    // All functions must ALWAYS return a value
     return 0;
 }
--- 02_loops_and_conditions/main.cpp
+++ 02_loops_and_conditions/main.cpp
@@ -4,11 +4,15 @@ using std::cin;
 
 
 int main() {
+    // As previously mentioned, variable declaration do not require us to assign a value in cpp.
     int firstNumber;
     int secondNumber;
     bool continueExecuting = true;
     int answer;
 
+    // while loops are always declared with the keyword while followed by parentheses in which is
+    // passed the condition to evaluate for the continuity of the loop, this can be a variable,
+    // like here, or a boolean statement, such as an equality, etc.
     while(continueExecuting)
     {
         cout << "Enter your first number : ";
@@ -16,6 +20,9 @@ int main() {
         cout << "Enter your second number : ";
         cin >> secondNumber;
 
+        // if statements are always declared using the if keyword followed by parentheses, between
+        // which the condition is to be declared, this is similar to the while statement mentioned
+        // previously
         if (firstNumber > secondNumber)
         {
             cout << "The first number is greater than the second number." << std::endl;
@@ -32,6 +39,8 @@ int main() {
         cout << "Do you wish to keep comparing numbers? Type 0 for no, 1 for yes.";
         cin >> answer;
 
+        // As is the case in python, make sure that the condition to exit the while loop is properly
+        // modified inside the loop to avoid infinite loops
         if (answer == 0)
         {
             continueExecuting = false;
